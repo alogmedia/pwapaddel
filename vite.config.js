fee9124dff2585f24542cd54d6a69a36c5a6f1a4
allo-mir <allo@mir-robots.com>
2024-11-12 16:19:00 +0100

    fix: assets

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,50 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'pwapaddel',
-      short_name: 'pwapaddel',
-      description: 'Paddelshoppen PWA',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: "pwapaddel",
+        short_name: "pwapaddel",
+        description: "Paddelshoppen PWA",
+        theme_color: "#ffffff",
+      },
+      icons: [
+        {
+          src: "icon192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icon512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
-
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
